a2f61cf66441a9715be5905b0c938cc791e92990
Ibrahim Ahmed <ibrahim21383@gmail.com>
2020-02-25 17:26:33 +0200

    fixing the hotel book inner request class

--- src/Request/Inner/HotelBookInnerRequestBuilder.php
+++ src/Request/Inner/HotelBookInnerRequestBuilder.php
@@ -14,47 +14,44 @@ class HotelBookInnerRequestBuilder extends InnerRequestBuilder
 {
     protected function build($data)
     {
-        $this->xml->setById('client_reference_number',$data['client_reference_number']);
-        $this->xml->setById('guest_nationality',$data['guest_nationality']);
         $guestDataNodes = $this->guestNodes();
-        $guestNode = $this->xml->cloneElementById('guest');
-        $guestsNode = $this->xml->getElementById('guests');
+        $guestNode = $this->xml->cloneElementById('Guest');
+        $guestsNode = $this->xml->getElementById('Guests');
         $guestsNode->removeAttribute('id');
-        foreach ($data['guests'] as $guest) {
+        foreach ($data['Guests'] as $guest) {
             $guestsNode->appendChild($this->fillDataNodes($guest, $guestDataNodes, $guestNode));
         }
-        $this->xml->setElementAttributesById(['VoucherBooking' => $data['voucher_booking']], 'payment_info');
-        $this->xml->setById('session_id', $data['session_id']);
-        $this->xml->setById('no_of_rooms', $data['no_of_rooms']);
-        $this->xml->setById('result_index', $data['result_index']);
-        $this->xml->setById('hotel_code', $data['hotel_code']);
-        $this->xml->setById('hotel_name', $data['hotel_name']);
+        unset($data['Guests']);
+        $this->xml->setElementAttributesById(['VoucherBooking' => $data['PaymentInfo']], 'PaymentInfo');
+        unset($data['PaymentInfo']);
         $hotelRoomDataNodes = $this->hotelRoomNodes();
         $hotelRoomNode = $this->xml->cloneElementById('hotel_room');
         $hotelRoomsNode = $this->xml->getElementById('hotel_rooms');
         $hotelRoomsNode->removeAttribute('id');
-        foreach ($data['hotel_rooms'] as $hotel_room) {
+        foreach ($data['HotelRooms'] as $hotel_room) {
             $hotelRoomsNode->appendChild($this->fillDataNodes($hotel_room, $hotelRoomDataNodes, $hotelRoomNode));
         }
+        unset($data['HotelRooms']);
+        $this->setDataToXML($data);
     }
 
     private function guestNodes()
     {
-        $title = $this->xml->cloneElementById('title');
-        $first_name = $this->xml->cloneElementById('first_name');
-        $last_name = $this->xml->cloneElementById('last_name');
-        $age = $this->xml->cloneElementById('age');
-        return compact('title', 'first_name', 'last_name', 'age');
+        $Title = $this->xml->cloneElementById('Title');
+        $FirstName = $this->xml->cloneElementById('FirstName');
+        $LastName = $this->xml->cloneElementById('LastName');
+        $Age = $this->xml->cloneElementById('Age');
+        return compact('Title', 'FirstName', 'LastName', 'Age');
     }
 
     private function hotelRoomNodes()
     {
-        $room_index = $this->xml->cloneElementById('room_index');
-        $room_type_name = $this->xml->cloneElementById('room_type_name');
-        $room_type_code = $this->xml->cloneElementById('room_type_code');
-        $rate_plan_code = $this->xml->cloneElementById('rate_plan_code');
-        $room_rate = $this->xml->cloneElementById('room_rate');
-        $supplements = $this->xml->cloneElementById('supplements');
+        $room_index = $this->xml->cloneElementById('RoomIndex');
+        $room_type_name = $this->xml->cloneElementById('RoomTypeName');
+        $room_type_code = $this->xml->cloneElementById('RoomTypeCode');
+        $rate_plan_code = $this->xml->cloneElementById('RatePlanCode');
+        $room_rate = $this->xml->cloneElementById('RoomRate');
+        $supplements = $this->xml->cloneElementById('Supplements');
         return compact('room_index', 'room_type_name', 'room_type_code', 'rate_plan_code', 'room_rate', 'supplements');
     }
 
@@ -62,31 +59,38 @@ class HotelBookInnerRequestBuilder extends InnerRequestBuilder
     {
         /** @var \DOMElement $newParentNode */
         $newParentNode = $parentNode->cloneNode();
-        if (!empty($data['attributes'])) {
-            $this->xml->setElementAttributes($data['attributes'], $newParentNode);
-        }
         foreach ($dataNodes as $id => $dataNode) {
-            if (($id === 'age' && empty($data['age'])) || ($id === 'supplements' && empty($data['supplements']))) {
-                continue;
-            }
             /** @var \DOMElement $newDataNode */
             $newDataNode = $dataNode->cloneNode();
-            if ($id === 'supplements') {
-                foreach ($data['supplements'] as $supplement) {
-                    $supplementNode = $this->xml->createElement('SuppInfo');
-                    $this->xml->setElementAttributes($supplement, $supplementNode);
-                    $newDataNode->appendChild($supplementNode);
-                }
-                $newParentNode->appendChild($newDataNode);
-                continue;
+            switch ($id){
+                case 'Age':
+                    if(!empty($data['Age'])){
+                        $newDataNode->textContent = $data[$id];
+                        $newParentNode->appendChild($newDataNode);
+                    }
+                    break;
+                case 'Supplements':
+                    foreach ($data['Supplements'] as $supplement) {
+                        $supplementNode = $this->xml->createElement('SuppInfo');
+                        $this->xml->setElementAttributes($supplement, $supplementNode);
+                        $newDataNode->appendChild($supplementNode);
+                    }
+                    $newParentNode->appendChild($newDataNode);
+                    break;
+                case 'RoomRate':
+                    $this->xml->setElementAttributes($data['RoomRate'], $newDataNode);
+                    $newParentNode->appendChild($newDataNode);
+                    break;
+                case 'GuestInRoom':
+                case 'GuestType':
+                case 'LeadGuest':
+                    $newParentNode->setAttribute($id,$data[$id]);
+                    break;
+                default:
+                    $newDataNode->textContent = $data[$id];
+                    $newParentNode->appendChild($newDataNode);
+                    break;
             }
-            if ($id === 'room_rate') {
-                $this->xml->setElementAttributes($data['room_rate'], $newDataNode);
-                $newParentNode->appendChild($newDataNode);
-                continue;
-            }
-            $newDataNode->textContent = $data[$id];
-            $newParentNode->appendChild($newDataNode);
         }
         return $newParentNode;
     }
@@ -107,19 +111,19 @@ class HotelBookInnerRequestBuilder extends InnerRequestBuilder
                 </hot:Guests>     
                 <hot:PaymentInfo id="PaymentInfo" VoucherBooking="" PaymentModeType="Limit"> 
                 </hot:PaymentInfo>       
-                <hot:SessionId id="session_id"></hot:SessionId>
-                <hot:NoOfRooms id="no_of_rooms"></hot:NoOfRooms>       
-                <hot:ResultIndex id="result_index"></hot:ResultIndex>       
-                <hot:HotelCode id="hotel_code"></hot:HotelCode>       
-                <hot:HotelName id="hotel_name"></hot:HotelName>       
-                <hot:HotelRooms id="hotel_rooms">         
-                    <hot:HotelRoom id="hotel_room">           
-                        <hot:RoomIndex id="room_index"></hot:RoomIndex>           
-                        <hot:RoomTypeName id="room_type_name"></hot:RoomTypeName>           
-                        <hot:RoomTypeCode id="room_type_code"></hot:RoomTypeCode>           
-                        <hot:RatePlanCode id="rate_plan_code"></hot:RatePlanCode>           
-                        <hot:RoomRate id="room_rate" RoomFare="" RoomTax="" TotalFare=""/> 
-                        <hot:Supplements id="supplements">
+                <hot:SessionId id="SessionId"></hot:SessionId>
+                <hot:NoOfRooms id="NoOfRooms"></hot:NoOfRooms>       
+                <hot:ResultIndex id="ResultIndex"></hot:ResultIndex>       
+                <hot:HotelCode id="HotelCode"></hot:HotelCode>       
+                <hot:HotelName id="HotelName"></hot:HotelName>       
+                <hot:HotelRooms id="HotelRooms">         
+                    <hot:HotelRoom id="HotelRoom">           
+                        <hot:RoomIndex id="RoomIndex"></hot:RoomIndex>           
+                        <hot:RoomTypeName id="RoomTypeName"></hot:RoomTypeName>           
+                        <hot:RoomTypeCode id="RoomTypeCode"></hot:RoomTypeCode>           
+                        <hot:RatePlanCode id="RatePlanCode"></hot:RatePlanCode>           
+                        <hot:RoomRate id="RoomRate" RoomFare="" RoomTax="" TotalFare=""/> 
+                        <hot:Supplements id="Supplements">
                         </hot:Supplements>
                     </hot:HotelRoom> 
                 </hot:HotelRooms> 
